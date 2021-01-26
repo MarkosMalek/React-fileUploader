b9f690ef58fb7af4e3ae3b4886114748e698b0e8
Markos Malek <markosmalek3@gmail.com>
2021-01-26 10:13:43 +0200

    initial commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const fileUpload = require("express-fileupload");
+
+const app = express();
+app.use(fileUpload());
+
+//upload Endpoint
+app.post("/upload", (req, res) => {
+  if (req.files === null) {
+    return res.status(400).json({ msg: "No files has been Uploaded" });
+  }
+  const file = req.files.file;
+  file.mv(`/${__dirname}/client/public/uploads/${file.name}`, (err) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send(err);
+    }
+    res.json({ fileName: file.name, filePath: `/uploads/${file.name}` });
+  });
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`server is running at port ${PORT}`));
