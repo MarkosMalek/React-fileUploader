fdc9545f0afc97749a49b963c0fbbf08acedf712
Markos Malek <markosmalek3@gmail.com>
2021-01-29 13:06:24 +0200

    third update

--- server.js
+++ server.js
@@ -10,11 +10,12 @@ app.post("/upload", (req, res) => {
     return res.status(400).json({ msg: "No files has been Uploaded" });
   }
   const file = req.files.file;
-  file.mv(`/${__dirname}/client/public/uploads/${file.name}`, (err) => {
+  file.mv(`${__dirname}/client/public/uploads/${file.name}`, (err) => {
     if (err) {
       console.error(err);
       return res.status(500).send(err);
     }
+
     res.json({ fileName: file.name, filePath: `/uploads/${file.name}` });
   });
 });
